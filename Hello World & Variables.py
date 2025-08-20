409b4185c0ae44d7829abd7d29fd2eb16ba11508
razibdash <avrorazib@gmail.com>
2025-08-20 19:31:47 +0600

    Create a program that stores your name, age, and favorite color in variables and then prints them out

---
+++ Hello World & Variables.py
@@ -0,0 +1,10 @@
+#Write a Python program that prints "Hello, World!" to the screen.
+print("Hello, World!")
+
+#Create a program that stores your name, age, and favorite color in variables and then prints them out.
+
+def storeInfo(name,age,favoriteColor):
+     print(name,age,favoriteColor)
+storeInfo("Razib Dash",24,'Black')
+
+
