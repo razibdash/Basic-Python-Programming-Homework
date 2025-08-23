c38ceaa5ca74a288cab0790ade0232d3e4e89db3
razibdash <avrorazib@gmail.com>
2025-08-24 00:19:19 +0600

    Create a list of 5 different fruits and print the second item from the list.Convert the list into a tuple and print it.

---
+++ Lists & Tuples.py
@@ -0,0 +1,12 @@
+# 1.Create a list of 5 different fruits and print the second item from the list.
+
+# 3.Write a program that finds the largest and smallest number from a list of integers.
+
+fruits=['apple','banana','mango','orange','pinaple']
+
+print(fruits[1])
+
+# 2.Convert the list into a tuple and print it.
+
+tuple_fruits=tuple(fruits)
+print("Fruits tuple:", tuple_fruits)
